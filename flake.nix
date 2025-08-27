54cb745d85eb9cd055b3a0d84f2cf7c83bb6d356
AzurIce <973562770@qq.com>
2025-08-27 10:48:41 +0800

    chore: added flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "My personal website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            zola
+            bun
+          ];
+        };
+      }
+    );
+}
