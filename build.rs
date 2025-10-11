196c2bf3d5d7cd1377610ae98879fefdfd410926
AzurIce <973562770@qq.com>
2025-10-11 12:46:22 +0800

    chore: udpate aoike

--- build.rs
+++ build.rs
@@ -1,22 +1,34 @@
-use aoike::build::utils::inject_str;
+use std::path::Path;
+
+use aoike::build::{Entity, post::Post, utils::inject_str};
 
 fn main() {
     println!("cargo:rerun-if-changed=docs");
 
-    aoike_sycamore::build::init_aoike_sycamore();
+    if !Path::new("static/css").exists() {
+        aoike_sycamore::build::init_aoike_sycamore();
+    }
 
     // Parse markdown files to HTML using aoike-build
     let posts = aoike::build::parse_posts("docs/posts");
-    let index = aoike::build::parse_post("docs/index.md");
+    let index = Entity::new("docs/index.md");
+    let index = Post::try_from(index).unwrap();
 
     let assets = aoike::build::get_assets_trunk_data(&posts, &index, "docs");
     let index_html = std::fs::read_to_string("index.html").unwrap();
-    std::fs::write(
-        "index.html",
-        inject_str(&index_html, &assets, "AOIKE_SYCAMORE_SITE_ASSETS", Some("</head>")),
-    )
-    .unwrap();
+    let injected_index_html = inject_str(
+        &index_html,
+        &assets,
+        "AOIKE_SYCAMORE_SITE_ASSETS",
+        Some("</head>"),
+    );
+    if index_html != injected_index_html {
+        std::fs::write("index.html", injected_index_html).unwrap();
+    }
     let out_dir = std::env::current_dir().unwrap().join("src");
-    let code = aoike::build::generate_code(posts, index);
-    std::fs::write(out_dir.join("docsgen.rs"), code).unwrap();
+    let code = std::fs::read_to_string(out_dir.join("docsgen.rs")).unwrap_or(String::new());
+    let gen_code = aoike::build::generate_code(posts, index);
+    if code != gen_code {
+        std::fs::write(out_dir.join("docsgen.rs"), gen_code).unwrap();
+    }
 }
--- docs/index.md
+++ docs/index.md
@@ -1,7 +1,9 @@
 Hello！这里是 Azur冰弦（AzurIce），一个热爱音乐、动漫、代码和游戏的二次元🥰
 
-是的，没错，我又又又换了博客框架，这次是基于 `build.rs` 和 ~[DioxusLabs/dioxus](https://github.com/DioxusLabs/dioxus)~（现在改成 [sycamore-rs/sycamore](https://github.com/sycamore-rs/sycamore) 了） 手搓的（Rust 赛高🥰）。
+是的，没错，我又又又换了博客框架，这次是基于 `build.rs` 和 ~~[DioxusLabs/dioxus](https://github.com/DioxusLabs/dioxus)~~（现在改成 [sycamore-rs/sycamore](https://github.com/sycamore-rs/sycamore) 了） 手搓的（Rust 赛高🥰）。
 本来想用 [getzola/zola](https://github.com/getzola/zola) 来着，但是我发现它的模板系统比较麻烦，而且一堆的配置也对写东西侵入性比较强，所以就自己写了一个。
+
+简单来说就是用 `build.rs` 解析文件（比如 `.md` 以及 `.typ`）然后 codegen 到 sycamore 的 Rust 代码。
 
 因为是拿来放一堆文章笔记的，所以叫它「池」，又因为喜欢蓝色，所以叫它「青池」，这里是仓库 [AzurIce/aoike](https://github.com/AzurIce/aoike)，不过目前还非常的 experimental。
 
